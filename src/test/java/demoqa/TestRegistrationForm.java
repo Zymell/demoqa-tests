f8874eacef3124873026d12037efef93a7761f0f
Zymell <sakh_22@mail.ru>
2022-04-03 10:34:43 +0500

    Complite registration Form Test

--- src/test/java/demoqa/TestRegistrationForm.java
+++ src/test/java/demoqa/TestRegistrationForm.java
@@ -5,21 +5,22 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 
-public class  TestRegistrationForm {
+public class TestRegistrationForm {
     @BeforeAll
-    static void BeforeAll(){
+    static void BeforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
     }
 
     @Test
-    void parameters(){
+    void parameters() {
         String First_name = "Alexander";
         open("/automation-practice-form");
 
@@ -32,14 +33,22 @@ public class  TestRegistrationForm {
         $(".react-datepicker__month-select").selectOption("October"); //Выбор месяца
         $(".react-datepicker__year-select").selectOption("1993"); //Выбор года
         $("[aria-label$='October 27th, 1993']").click();
-        $("[id=subjectsInput]").setValue("English").pressEnter();
-        $(byText("Sports")).click();
-        $("[id=uploadPicture]").uploadFromClasspath("Lesson_2.png");
-        $("[id=currentAddress]").setValue("Russia Stalina streat 37");
-        $("[id=currentAddress]").setValue("Russia Stalina streat 37");
-        $("[id=currentAddress]").setValue("Russia Stalina streat 37");
+        $("[id=subjectsInput]").setValue("English").pressEnter(); //Выбор предмета
+        $(byText("Sports")).click(); //Выбор хобби
+        $("[id=uploadPicture]").uploadFromClasspath("Lesson_2.png"); //Выбор картинки
+        $("[id=currentAddress]").setValue("Stalin street 37"); //Ввод улицы
+        $("[id=state]").click(); // Выбор государства
+        $(byText("NCR")).click();
+        $("[id=city]").click(); // Выбор города
+        $(byText("Delhi")).click();
+        $("[id=submit]").click(); //Подтверждение регистрации
 
+        $(".table-responsive").shouldHave(text("Student Name	" + First_name + " Popov"), //Проверка формы регистрации
+                text("Mobile 8952724578"), text("Picture Lesson_2.png"), text("Student Email example1993@gmail.com"),
+                text("Gender Male"), text("Date of Birth 27 October,1993"), text("Subjects English"),
+                text("Hobbies Sports"), text("Address Stalin street 37"), text("State and City NCR Delhi"));
 
+        $("#closeLargeModal").click();
 
 
     }
