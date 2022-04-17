6b5db57964a2d96945343a1498bcd8642f80cb45
Zymell <sakh_22@mail.ru>
2022-04-17 21:51:59 +0500

    Added PageObject Test

--- src/test/java/demoqa/TestRegistrationFormWithPageObjects.java
+++ src/test/java/demoqa/TestRegistrationFormWithPageObjects.java
@@ -2,8 +2,6 @@ package demoqa;
 
 import com.codeborne.selenide.Configuration;
 import demoqa.page.RegistrationPage;
-import com.github.javafaker.Faker;
-import demoqa.page.CalendarComponent;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -11,23 +9,6 @@ import org.junit.jupiter.api.Test;
 
 public class TestRegistrationFormWithPageObjects {
     RegistrationPage registrationPage = new RegistrationPage();
-    Faker faker = new Faker();
-    CalendarComponent calendar = new CalendarComponent();
-
-    String  firstName = faker.name().firstName(),
-            lastName = faker.name().lastName(),
-            userEmail = faker.internet().emailAddress(),
-            gender = faker.demographic().sex(),
-            userNumber = faker.numerify("###########"),
-            day = "27",
-            month = "October",
-            year = "1993",
-            subject = "English",
-            hobbies = "Sports",
-            CurrenAdress = faker.address().streetAddress(),
-            foto = "Lesson_2.JPG",
-            state = "NCR",
-            city = "Delhi";
 
     @BeforeAll
     static void BeforeAll() {
@@ -40,20 +21,21 @@ public class TestRegistrationFormWithPageObjects {
     void parameters() {
 
         registrationPage.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setUseremail(userEmail)
-                .setGender(gender)
-                .setNumber(userNumber)
-                .setDateOfBirth(day,month,year)
-                .setSubject(subject)
-                .setHobbies(hobbies)
-                .setUploadPicture(foto)
-                .setCurrentAddress(CurrenAdress)
-                .setState(state)
-                .setCity(city)
-                .setSubmit();
-        registrationPage.chekResult("Student name",firstName);
+                .setFirstName()
+                .setLastName()
+                .setUseremail()
+                .setGender()
+                .setNumber()
+                .setDateOfBirth()
+                .setSubject()
+                .setHobbies()
+                .setUploadPicture()
+                .setCurrentAddress()
+                .setState()
+                .setCity()
+                .setSubmit()
+
+                .chekResult();
 
 
 
