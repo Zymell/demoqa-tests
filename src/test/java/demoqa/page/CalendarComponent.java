7e9b09c743be2ad930e4afee7e3a3dff40d0d6c4
Zymell <sakh_22@mail.ru>
2022-04-17 21:50:23 +0500

    Added CalendarComponent

--- src/test/java/demoqa/page/CalendarComponent.java
+++ src/test/java/demoqa/page/CalendarComponent.java
@@ -3,10 +3,10 @@ package demoqa.page;
 import static com.codeborne.selenide.Selenide.$;
 
 public class CalendarComponent {
-    public void SetDate(String day, String month, String year){
+    public void setDateOfBirth(String day, String month, String year) {
         $(".react-datepicker__month-select").selectOption(month);
         $(".react-datepicker__year-select").selectOption(year);
-        $(".react-datepicker_day--0" + day +
-                ":not(.react-datepicker_day--outside-month)").click();
-       }
+        $(".react-datepicker__day--0" + day +
+                ":not(.react-datepicker__day--outside-month)").click();
+    }
 }
